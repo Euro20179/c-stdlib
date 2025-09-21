600f622d9dd19bf8e0bae83f6d22fabd13f5a271
euro20179 <sandollar2020@gmail.com>
2025-09-20 21:31:41 -0700

    feat(array): start on a basic array

---
+++ array.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include "array.h"
+
+array* array_new2(size_t cap, size_t item_size)
+{
+    array* arr = malloc(sizeof(array));
+    arr->len = 0;
+
+    if(cap == 0) cap = 1;
+
+    arr->cap = cap;
+    arr->item_size = item_size;
+    arr->data = calloc(cap, item_size);
+    return arr;
+}
+
+void array_del2(array* arr)
+{
+    free(arr->data);
+    free(arr);
+}
+
+int array_append(array* arr, void* data) {
+    if (arr->len == arr->cap) {
+        void* buf = reallocarray(arr->data, arr->cap * 2, arr->item_size);
+        if (buf == NULL) {
+            return -1;
+        }
+        arr->data = buf;
+
+        arr->cap *= 2;
+    }
+
+    memcpy(arr->data + arr->len * arr->item_size, data, arr->item_size);
+    arr->len++;
+
+    return 0;
+}
+
+void* array_at(array* arr, size_t idx) {
+    if(arr->len == 0) {
+        return NULL;
+    }
+    if (idx >= arr->len) {
+        return NULL;
+    }
+    return arr->data + idx * arr->item_size;
+}
---
+++ array.h
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+typedef struct {
+    size_t len;
+    size_t cap;
+    size_t item_size;
+    void* data;
+} array;
+
+///creates a new array and returns the pointer
+///caller must call array_del
+array* array_new2(size_t cap, size_t item_size);
+
+void array_del2(array*);
+
+///returns -1 on fail
+int array_append(array*, void* data);
+
+///returns NULL on out of bounds
+void* array_at(array*, size_t idx);
